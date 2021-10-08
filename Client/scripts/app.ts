be4b9b809d0900e0d3a53ef46916a07608beaab4
PriyankaKediya <prikediya@gmail.com>
2021-10-08 21:52:29 +0530

    added bootstrap and font-awesome support

---
+++ Client/scripts/app.ts
@@ -0,0 +1,13 @@
+//IIFE Immediately Invoked Function Express
+
+"use strict";
+(function(){
+
+    function Start():void
+    {
+        console.log("App started");
+    }
+
+    window.addEventListener("load", Start);
+    
+})();
