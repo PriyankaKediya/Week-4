5c64bf8ff734ff2b1e0771837b05a041018e22eb
PriyankaKediya <prikediya@gmail.com>
2021-10-17 23:19:27 +0530

    initial commit

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ app.use(function (err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
     res.status(err.status || 500);
-    res.render('error');
+    res.render('error', {title: 'Error'});
 });
 //# sourceMappingURL=app.js.map
